00f2e75bb3e31a0f25597241734834cc9550db81
Chris <chris.baumgartner115@gmail.com>
2022-07-31 18:09:42 +0200

    tests

---
+++ MathObjects/DimensionMismatchException.java
@@ -0,0 +1,10 @@
+package MathObjects;
+
+public class DimensionMismatchException extends Exception {
+
+    public DimensionMismatchException() {}
+
+    public DimensionMismatchException(String message) {
+        super(message);
+    }
+}
--- MathObjects/MatrixTest.java
+++ MathObjects/MatrixTest.java
@@ -11,23 +11,88 @@ public class MatrixTest {
 
     Matrix A = new Matrix(3, 3, Arrays.asList(1.,2.,3.,4.,5.,6.,7.,8.,9.));
     Matrix B = new Matrix(3, 3, Collections.emptyList(), 1.);
-    Matrix E = new Matrix(3, 3, Collections.emptyList());
+    Matrix C = new Matrix(3, 3, Collections.emptyList());
+    Matrix D = new Matrix(3, 3, Arrays.asList(9.,8.,7.,6.,5.,4.,3.,2.,1.));
+    /* TODO: fix tests again:
+    *   do multiplication with A and D and update assert values...*/
+    @Test
+    @DisplayName("Constructors work")
+    void constructorsTest() {
+        assertEquals(5., A.getElement(2, 2));
+        assertEquals(1., B.getElement(1, 2));
+        assertEquals(0., C.getElement(2, 1));
+    }
 
     @Test
     @DisplayName("setElement, getElement, and setAllElements work")
     void setMethodsTest() {
         Matrix E = new Matrix(3, 3, Collections.emptyList());
         E.setElement(2, 2, 9.);
-        assertEquals(2., E.getElement(2, 2));
+        assertEquals(9, E.getElement(2, 2));
+        E.set // TODO: test setAllElements()
     }
 
     @Test
-    @DisplayName("adding matrices and scalars to matrix works")
+    @DisplayName("Adding matrices and scalars to matrix works")
     void addTest() {
+        Matrix result1 = A;
         try {
-            Matrix result1 = A.add(B);
+            result1 = A.add(A);
         } catch (Exception ignored) {}
 
-        // TODO
+        assertEquals(2, result1.getElement(1,1));
+        assertEquals(10, result1.getElement(2,2));
+
+        Matrix result2 = A;
+        try {
+            result2 = A.add(3);
+        } catch (Exception ignored) {}
+
+        assertEquals(4, result2.getElement(1,1));
+        assertEquals(8, result2.getElement(2,2));
     }
+
+    @Test
+    @DisplayName("Subtracting matrices and scalars from matrices works")
+    void subtractTest() {
+        Matrix result1 = A;
+        try {
+            result1 = A.subtract(); //TODO: fix test again
+        } catch (Exception ignored) {}
+
+        assertEquals(0, result1.getElement(1,1));
+        assertEquals(4, result1.getElement(2,2));
+
+        Matrix result2 = A;
+        try {
+            result2 = A.subtract(3);
+        } catch (Exception ignored) {}
+
+        assertEquals(-2, result2.getElement(1,1));
+        assertEquals(2, result2.getElement(2,2));
+    }
+
+    @Test
+    @DisplayName("Multiplying with matrices and scalars to matrices works")
+    void multiplyTest() {
+        Matrix result1 = A;
+        try {
+            result1 = A.multiply(A);
+        } catch (Exception ignored) {}
+
+        assertEquals(30, result1.getElement(1,1));
+        assertEquals(81, result1.getElement(2,2));
+        assertEquals(150, result1.getElement(3,3));
+
+        Matrix result2 = A;
+        try {
+            result2 = A.multiply(3);
+        } catch (Exception ignored) {}
+
+        assertEquals(3, result2.getElement(1,1));
+        assertEquals(15, result2.getElement(2,2));
+    }
+
+    // TODO: test invert()
+
 }
